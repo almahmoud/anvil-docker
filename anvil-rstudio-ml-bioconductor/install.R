3d967dbc9a71bd3a567f7d59a9384b554593538a
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-02-14 14:28:58 -0500

    Add ml

---
+++ anvil-rstudio-ml-bioconductor/install.R
@@ -0,0 +1,14 @@
+# Install Bioconductor packages
+BiocManager::install(c(
+    "AnVIL",
+    "SingleCellExperiment", 
+    "GenomicFeatures", 
+    "GenomicAlignments", 
+    "ShortRead", 
+    "DESeq2", 
+    "AnnotationHub", 
+    "ExperimentHub", 
+    "ensembldb", 
+    "scRNAseq", 
+    "scran", 
+    "Rtsne"))
